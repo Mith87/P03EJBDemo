0016f035905fe5749192a26ae5f30f9c211c1f05
josue <josueperez.sol@gmail.com>
2014-02-02 03:07:43 -0600

    agrego atributos y servicios a Person.java y PersonPOJO.java

--- ejemplo/src/main/java/ejemplo/ejb/Person.java
+++ ejemplo/src/main/java/ejemplo/ejb/Person.java
@@ -23,6 +23,10 @@ public class Person implements Serializable{
 	
 	private String nombre;
 	private String apellido;
+	private String cedula;
+	private String telefono;
+	private String email;
+	
 	
 	public Person() {
 		super();
@@ -52,5 +56,28 @@ public class Person implements Serializable{
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+	
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
 
